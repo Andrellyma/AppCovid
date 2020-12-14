687df8f4e927fb291526401c9398fd5dc811d0b1
andrellyma <32910778+Andrellyma@users.noreply.github.com>
2020-12-14 14:02:41 -0300

    close #7 Criação da Tela Inicial do App

---
+++ app/src/main/java/com/example/appcovid/BuscarEstadoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appcovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BuscarEstadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar_estado);
+    }
+}
--- app/src/main/java/com/example/appcovid/MainActivity.java
+++ app/src/main/java/com/example/appcovid/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appcovid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void startBuscarPaisActivity(View view){
+        Intent Acitivity = new Intent(this,BuscarPaisActivity.class);
+        startActivity(Acitivity);
+    }
+    public void startBuscarEstadoActivity(View view){
+        Intent Acitivity = new Intent(this,BuscarEstadoActivity.class);
+        startActivity(Acitivity);
+    }
 }
+
