8429514645c75d13382eca4c7ff18f6058886e6a
andrellyma <32910778+Andrellyma@users.noreply.github.com>
2020-12-18 13:21:45 -0300

    close #8 Tela Busca por estado finalizada

--- app/src/main/java/com/example/appcovid/BuscarEstadoActivity.java
+++ app/src/main/java/com/example/appcovid/BuscarEstadoActivity.java
@@ -3,12 +3,23 @@ package com.example.appcovid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class BuscarEstadoActivity extends AppCompatActivity {
+    Spinner estados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_estado);
+
+        //Adicionando as String dentro do spinner
+        estados = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.estados,android.R.layout.simple_spinner_item);
+        estados.setAdapter(adapter);
+
     }
+
+
 }
